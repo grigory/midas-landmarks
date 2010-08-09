7f3ffb46a3a2f1af436b19de120a8f48ebd4d2db
Grigory Yaroslavtsev <grigory@inbox.ru>
2010-08-09 21:21:59 +0400

    Version for parameter tweaking.

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+make
+./midas.exe -generate $1 -pool 20000 -randc 10 -upper 10 -points 2048 -t 100 -radius 1000 -seed 123 > landmark.lmk
+./midas.exe -evaluate $1 -seed 566 < landmark.lmk
+#./midas.exe -evaluate bay.gr -seed 346 < landmark.lmk
+#./midas.exe -evaluate bay.gr -seed 610 < landmark.lmk
+#./midas.exe -evaluate bay.gr -seed 22 < landmark.lmk
+
